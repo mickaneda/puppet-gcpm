a3a094f78598268bdb13b5b03f04f4bd0680fcd0
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-01-19 12:29:57 +0900

    use data for YAML

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,36 +1,12 @@
 class gcpm::config {
-  $config_dir   = $gcpm::config_dir
-  $oatuh_file   = $gcpm::oatuh_file
-  $service_account_file = $gcpm::service_account_file
-  $project      = $gcpm::project
-  $zone         = $gcpm::zone
-  $max_cores    = $gcpm::max_cores
-  $machines     = $gcpm::machines
-  $static_wns   = $gcpm::static_wns
-  $required_machines = $gcpm::required_machines
-  $prefix       = $gcpm::prefix
-  $preemptible  = $gcpm::preemptible
-  $off_timer    = $gcpm::off_timer
-  $network_tag  = $gcpm::network_tag
-  $reuse        = $gcpm::reuse
-  $interval     = $gcpm::interval
-  $head_info    = $gcpm::head_info
-  $head         = $gcpm::head
-  $port         = $gcpm::port
-  $domain       = $gcpm::domain
-  $admin        = $gcpm::admin
-  $owner        = $gcpm::owner
-  $wait_cmd     = $gcpm::wait_cmd
-  $bucket       = $gcpm::bucket
-  $storageClass = $gcpm::storageClass
-  $location     = $gcpm::location
-  $log_file     = $gcpm::log_file
-  $log_level    = $gcpm::log_level
+  $data   = $gcpm::data
 
-  file { '/etc/gcpm.yml':
-    content => template("${module_name}/gcpm.yml.erb"),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+  if $data {
+    file { '/etc/gcpm.yml':
+      content => template("${module_name}/gcpm.yml.erb"),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
   }
 }
