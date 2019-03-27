052b7c99a7901dd31bacd4f5df34c9a33ccfba83
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-03-27 20:38:50 +0900

    separate service install/uninstall from service and put int install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,11 @@
 class gcpm::install {
   python::pip { 'gcpm' :
-    pkgname       => 'gcpm',
-    timeout       => 1800,
+    pkgname => 'gcpm',
+    timeout => 1800,
+    ensure  => 'present'
+    notify  => Exec['install_gcpm_service']
+  }
+  exec {"install_gcpm_service":
+    command => "/usr/bin/gcpm install",
   }
 }
