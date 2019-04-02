0e44f98f1473b01d7a8d464d6ecd99be16f2ca39
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-04-02 17:48:59 +0900

    pip installation

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,12 @@
 class gcpm::install {
+  package { 'python2-pip':
+    ensure => installed
+  }
   python::pip { 'gcpm' :
     pkgname => 'gcpm',
     timeout => 1800,
     ensure  => 'present',
+    require  => Package['python2-pip'],
     notify  => Exec['install_gcpm_service']
   }
   exec {"install_gcpm_service":
